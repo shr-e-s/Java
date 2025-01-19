9b54880a33f642a8376f5b53d91bf6108ce70acf
Shreyas M <shreyasm247@gmail.com>
2025-01-19 18:03:53 +0530

    Variables

---
+++ src/com/classOne/C1.java
@@ -0,0 +1,11 @@
+package com.classOne;
+
+//First Java program
+public class C1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello word !");
+    }
+
+}
---
+++ src/com/classOne/Datatype.java
@@ -0,0 +1,31 @@
+package com.classOne;
+
+//Datatype is used to define the type of data that a variable can hold
+
+public class Datatype {
+    //Java has 2 types of datatypes
+
+    //Primitive Datatype
+
+    //integer type
+    byte b = 50;
+    short s = 500;
+    int i = 14253678;
+    long l = 12345679L;
+
+    //decimal type
+    float f = 1.45f;
+    double d = 1.2456;
+
+    boolean t = true;
+    boolean fa = false;
+
+    char c = 'A';
+
+    //Non-primitive Datatype
+    //arrays
+    //collections
+    //interface
+    //class
+
+}
